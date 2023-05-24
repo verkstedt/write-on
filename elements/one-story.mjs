08ccb6292858578558b861abd42bd8160e032f33
Alexander Thomas <me@alexander-thomas.net>
2023-05-24 16:03:44 +0200

    Update one-story.mjs

--- elements/one-story.mjs
+++ elements/one-story.mjs
@@ -3,6 +3,13 @@ import { html } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.j
 import AppElement from '../utils/AppElement.mjs'
 import formatDate from '../utils/formatDate.mjs'
 
+function getInitials(name) {
+  const nameArray = name.split(" ")
+  const firstNameIn = nameArray[0].charAt(0).toUpperCase()
+  const lastNameIn = nameArray[nameArray.length - 1].charAt(0).toUpperCase()
+  return [ firstNameIn, lastNameIn ].join('')
+}
+
 class OneStory extends AppElement
 {
   static properties = {
@@ -35,14 +42,33 @@ class OneStory extends AppElement
         {
           filter: blur(0.5ex);
         }
+        cite {
+          display: inline-block;
+          background-color: var(--cemph);
+          margin-right: 25px;
+          border-radius: 50%;
+          float:left
+        }
+        
+        cite .inner {
+          color: white;
+          display: table-cell;
+          vertical-align: middle; 
+          text-align: center;
+          text-decoration: none;
+          height: 50px;
+          width: 50px;  
+          font-size: var(--rem);
+        }
+        blockquote {
+          border-left: none;
+        }
       </style>
       <section>
         ${this.story.map((part, idx) => html`
           <blockquote>
             <cite>
-              ${part.player}
-              at
-              ${formatDate(new Date(part.createdAt))}
+              <span class="inner">${getInitials(part.player)}</span>
             </cite>
             <p>
               <span class=${this.finished ? '' : 'censored'}>
